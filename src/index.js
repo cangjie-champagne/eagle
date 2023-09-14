1f84b1222ad436a32050fde2493cf4eb9f223a48
João Pedro <joaopjt@icloud.com>
2023-09-14 13:40:38 -0300

    added j() method values

--- src/index.js
+++ src/index.js
@@ -456,7 +456,7 @@ class Eagle {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['bow'],
       web: []
     };
     
@@ -472,19 +472,19 @@ class Eagle {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['female'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['moon'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['bow'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['hand'];
           break;
       }
     }
