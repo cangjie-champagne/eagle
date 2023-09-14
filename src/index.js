e7466635759afe9d7a193af49121d7e64bed392c
João Pedro <joaopjt@icloud.com>
2023-09-14 13:56:04 -0300

    added q() method values

--- src/index.js
+++ src/index.js
@@ -692,7 +692,7 @@ class Eagle {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['moon'],
       web: []
     };
     
@@ -708,19 +708,19 @@ class Eagle {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['fire'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['hand'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['people'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['field'];
           break;
       }
     }
